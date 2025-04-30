459da2955938e89a354f7e4957986eba48c1da6b
mahmoudrabbas <mahmoudrabass11@gmail.com>
2025-05-01 02:51:29 +0300

    first commit

---
+++ src/main/java/com/compedia/CompediaApplication.java
@@ -0,0 +1,13 @@
+package com.compedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compedia/CompediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
