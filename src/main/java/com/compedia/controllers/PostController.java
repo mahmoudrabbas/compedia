52352c2f74a312bedad37b3841b70da954ef125e
mahmoudrabbas <mahmoudrabass11@gmail.com>
2025-05-02 05:00:15 +0300

    add post controller

---
+++ src/main/java/com/compedia/controllers/PostController.java
@@ -0,0 +1,41 @@
+package com.compedia.controllers;
+
+import com.compedia.entities.PostEntity;
+import com.compedia.repositories.PostRepository;
+import com.compedia.services.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllPosts(){
+        return ResponseEntity.ok().body(postService.getAllPosts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id){
+        return ResponseEntity.ok().body(postService.getPostById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> add(PostEntity post){
+        return ResponseEntity.ok().body(postService.addPost(post));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(PostEntity post){
+        return ResponseEntity.ok().body(postService.updatePost(post));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        return ResponseEntity.ok().body(postService.deletePostById(id));
+    }
+
+}
